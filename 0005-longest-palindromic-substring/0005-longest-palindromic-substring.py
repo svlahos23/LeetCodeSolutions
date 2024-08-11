6716b5681ca830aff47bcff3fd53909a9be11a9f
svlahos23 <146478047+svlahos23@users.noreply.github.com>
2024-08-11 18:23:55 -0500

    Time: 70 ms (98.69%), Space: 16.6 MB (38.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,17 +4,13 @@ class Solution:
         n = len(T)
         P = [0] * n
         C = R = 0
-
         for i in range(1, n - 1):
-            P[i] = (R > i) and min(R - i, P[2 * C - i])  # Use previously computed values if within bounds
+            P[i] = (R > i) and min(R - i, P[2 * C - i])
             while T[i + P[i] + 1] == T[i - P[i] - 1]:
                 P[i] += 1
-
             if i + P[i] > R:
                 C, R = i, i + P[i]
-
-    # Find the maximum element in P
         max_len, center_index = max((n, i) for i, n in enumerate(P))
-        start = (center_index - max_len) // 2  # Map back to the original string
+        start = (center_index - max_len) // 2
 
         return s[start:start + max_len]
